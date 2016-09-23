647c77537d6d7fb248a17c3ecf0936da99eafd7f
Yoann <yoann.gloaguen@gmail.com>
2016-09-23 09:38:18 +0200

    ajout des dépendances composer etc...

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moimeme
+ * Date: 22/09/16
+ * Time: 10:36
+ */
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Neoxi</title>
+    </head>
+    <body>
+        <h2>NEOXI</h2>
+    </body>
+</html>
