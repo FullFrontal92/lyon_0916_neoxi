f5e9d83ef93e76a4b41d10d45a502b4ad3607d9b
Yoann <yoann.gloaguen@gmail.com>
2016-10-03 17:50:36 +0200

    Update du header, hamburger défaillant

--- public/header.php
+++ public/header.php
@@ -1,10 +1,27 @@
-<?php
-?>
-
-
-
- <header>
-    	
-    <h1>fxghcgff</h1>
-    	 
-       </header>
+<header>
+        <div class="navbar navbar-default navbar-fixed-top role="navigation">
+        <div class="header-background container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="align-inline col-xs-2 col-md-4">
+                    <img class="header-height" src="images/boussole.png"/>
+                    <div class="neoxi-header-title"><h1 class="nav"><a href="index.php">NEOXI</a></h1></div>
+                    <div class="center-height navbar-collapse collapse">
+                        <ul class="align-inline nav navbar-nav">
+                            <li><a href="index.php">LA&nbsp;SOCIÉTÉ</a></li>
+                            <li><a href="notre_expertise.php">EXPERTISE</a></li>
+                            <li><a href="nos_engagements.php">ENGAGEMENTS</a></li>
+                            <li><a href="logiciels_references.php">LOGICIELS</a></li>
+                            <li><a href="actu.php">ACTUALITÉS</a></li>
+                            <li><a href="index.php">CONTACT</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+</header>
--- public/index.php
+++ public/index.php
@@ -1,39 +1,29 @@
 
 <!DOCTYPE html>
 	
-	<html>
-    	
-    	<head>
-        	
-        	<meta charset="utf-8" />
-			
-			<link rel="stylesheet" type="text/css" href="index.css">
-        <!-- Latest compiled and minified CSS -->
-			
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        
-        	<title>Neoxi</title>
-
-    </head>
-    
-    <body>
-   
-   		<?php
-			include"header.php" 
-		?>
-
-
-    <?php
-		include"footer.php"
-		?>
-
-
-      
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<title>Neoxi</title>
+		<!--raleway font-->
+		<style>
+			@import 'https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=latin-ext';
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+		<link rel="stylesheet" type="text/css" href="css/header.css">
+	</head>
+	<body>
+   		<?php include"header.php" ?>
+		<div class ="row" style="height: 800px;">
+			<div class="col-xs-12">
+			</div>
+		</div>
+    	<?php include"footer.php" ?>
     </body>
 </html>
