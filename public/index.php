edb89a68cb22e942dcf9abea079fcf53cbdb0041
wafaworks <wafa.adj@gmail.com>
2016-10-11 10:44:18 +0200

    Deleted home.php reinserted in index.php - Finished index style in index.css - Not responsive yet (todo)

--- public/index.php
+++ public/index.php
@@ -24,9 +24,87 @@
 		<link rel="stylesheet" media="screen and (max-width: 931px)" type="text/css" href="css/queriesMd.css">
 	</head>
 	<body>
-   		<?php include"header.php" ?>
-        <?php include"home.php" ?>
-    	<?php include"footer.php" ?>
+   		<?php include"header.php"?>
+		<div class="container-fluid home" > <!-- home page image and text bloc -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-lg-12" id="homeimg">
+					<div class="bloc-texte" alt="">
+						<h2 id="title">NEOXI</h2>
+						<p>Spécialiste en logiciels ERP <br/>
+							Partenaire commercial et technique fiable </p>
+						<input type="button" title="cliquez" value="EN SAVOIR PLUS" id="home-button">
+					</div>
+				</div>
+			</div>
+			<div id="citation">
+				<p> NEOXI a été créée pour les revendeurs logiciels afin d’être <br/>
+					un partenaire commercial et technique fiable. </p>
+			</div>
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-lg-4" id="badge">
+					<img src="images/objectifs-homePage.png">
+					<h3 class="desc">OBJECTIFS</h3>
+					<p>Renforcer les équipes <br/> des revendeurs de logiciels <br/> de gestion</p>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-lg-4" id="badge2">
+					<img src="images/homePage-solution.png">
+					<h3 class="desc">SOLUTIONS</h3>
+					<p> Prestation sur mesure <br/>
+						Partenariat de proximité <br/>
+						Flexibilité et savoir-faire</p>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-lg-4" id="badge3">
+					<img src="images/specialist-homePage.png">
+					<h3 class="desc">SPECIALITÉS</h3>
+					<p>Logiciels de gestions du marché <br/> des P.M.E et M.G.E</p>
+				</div>
+			</div>
+
+			<div class="row bloc2">
+				<div class="last-news-bloc">
+					<h1 id="last-news">DERNIÈRES ACTUALITÉS</h1>
+				</div>
+				<div class="col-xs-4 col-sm-4 col-lg-4 news" >
+					<img src="images/actu1_home.jpg">
+					<h2>Première actualité</h2>
+					<p>Quae dum ita struuntur, indicatum est
+						apud Tyrum indumentum regale textum
+						occulte,incertum quo locante vel cuius
+						usibus apparatum. ideoque rector provinci
+						tunc pater Apollinaris eiusdem nominis ut
+						conscius [...]</p>
+					<p><img src="images/time-homePage-articles.png"> 30 Septembre 2016</p>
+                    <input type="button" title="cliquez" value="Lire la suite" id="news-button">
+				</div>
+				<div class="col-xs-4 col-sm-4 col-lg-4 news">
+					<img src="images/actu2_home.jpg">
+					<h2>Deuxième actualité</h2>
+					<p>Quae dum ita struuntur, indicatum est
+						apud Tyrum indumentum regale textum
+						occulte,incertum quo locante vel cuius
+						usibus apparatum. ideoque rector provinci
+						tunc pater Apollinaris eiusdem nominis ut
+						conscius [...]</p>
+					<p><img src="images/time-homePage-articles.png"> 30 Octobre 2016</p>
+                    <input type="button" title="cliquez" value="Lire la suite" id="news-button">
+
+				</div>
+				<div class="col-xs-4 col-sm-4 col-lg-4 news">
+					<img src="images/actu3_home.jpg">
+					<h2>Troisième actualité</h2>
+					<p>Quae dum ita struuntur, indicatum est
+						apud Tyrum indumentum regale textum
+						occulte,incertum quo locante vel cuius
+						usibus apparatum. ideoque rector provinci
+						tunc pater Apollinaris eiusdem nominis ut
+						conscius [...]</p>
+					<p><img src="images/time-homePage-articles.png"> 30 Novembre 2016</p>
+                    <input type="button" title="cliquez" value="Lire la suite" id="news-button">
+				</div>
+			</div>
+		</div>
+
+		<?php include"footer.php" ?>
 
     </body>
 </html>
