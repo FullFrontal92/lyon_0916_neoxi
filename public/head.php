1a9107c0127eb3e719eedb41701f914173b7cda7
florent <dabinflorent@gmail.com>
2016-10-13 18:09:37 +0200

    debuggage

--- public/head.php
+++ public/head.php
@@ -7,6 +7,7 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src="js/sticky.js"></script>
+<script type="text/javascript" src="js/actu.js"></script>
 <title>Neoxi</title>
 <!--raleway font-->
 <style>
