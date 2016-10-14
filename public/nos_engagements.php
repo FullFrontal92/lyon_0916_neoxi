afa9b7d39b0de10657f38cc48bd0b5e09ef75499
wafaworks <wafa.adj@gmail.com>
2016-10-14 05:30:45 +0200

    Created engagement.php and engagement.css. Almost done : to be responsive and light details to fix

--- public/nos_engagements.php
+++ public/nos_engagements.php
@@ -2,9 +2,74 @@
 <html>
 	<head>
 		<?php include"head.php" ?>
-	</head>
+    </head>
 	<body>
 		<?php include"header.php" ?>
+        <div class="container-fluid home" >
+            <div class="row">
+                <div class="col-xs-12" id="engImg">
+                    <div class="bloc-text-eng">
+                        <div class="center-text">
+                            <h2 id="title-log">NOS ENGAGEMENTS</h2>
+                            <p>Neoxi met à disposition ses ressources pour que vous puissiez
+                                les intégrer dans vos plannings opérationnels.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="engBox row">
+                    <div>
+                        <div class="col-xs-12 col-md-6">
+                           <img src="images/diagram.png" alt="diagramme" title="engagements neoxi">
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <p class="engText">
+                                De part sa nature, Neoxi n'est pas un concurrent
+                                mais apporteur de ressources complémentaires en terme d'effectif
+                                et apporteur de compétences que vous ne désirez pas acquérir.
+                                <br/>
+                                <br/>
+                                <br/>
+                                Neoxi s'engage à respecter la plus stricte confidentialité sur les informations fournies
+                                par les donneurs d'ordres et sur les sites sur lesquels elle intervient.
+                                <br/>
+                                <br/>
+                                <br/>
+                                Comme cela s'est plusieurs fois produit, Neoxi dans le cadre de ses interventions peut
+                                être force de proposition auprès de vos clients, en matière de nouvelles options,
+                                de nouveaux logiciels complémentaires ou de nouvelles plateformes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid row-brown">
+                <div class="sudEst row ">
+                    <div class="container">
+                        <div class="col-xs-12 titleLogs">LE SUD-EST : zone principale d'intervention</div>
+                        <div class="col-xs-12 col-md-6">
+                            <div class="sudText">
+                            <p>
+                                Nos consultants ont pour mission de se comporter comme de vrais consultants maison
+                                de votre société, avec remontées des informations commerciales et techniques pertinentes
+                                pour votre suivi client.
+                                La volonté de la société est de créer avec les donneurs d'ordres un vrai partenariat récurrent.
+                                <br/>
+                                <br/>
+                                <br/>
+                                Nos consultants ont pour mission de se comporter comme de vrais consultants maison de votre
+                                société, avec remontées des informations commerciales et techniques pertinentes pour votre suivi client.
+                            </p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <img src="images/compta.png" alt="comptabilité" title="engagements neoxi">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 		<?php include"footer.php" ?>
 	</body>
 </html>
