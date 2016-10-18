ab3f8b6b163d5a628fc0df892efb08b87c402bda
wafaworks <wafa.adj@gmail.com>
2016-10-18 16:30:03 +0200

    Created mention-legales.php and linked it in the footer mention

---
+++ public/mention-legales.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <?php include "head.php" ?>
+</head>
+
+<body>
+    <?php include "header.php" ?>
+    <div class="container-fluid esp">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 id="bloctxt">MENTIONS LÉGALES</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 col-xs-12">
+                <h2>Société éditrice du site</h2>
+                <p>Ce site web est la propriété de NEOXI</p>
+                <p>S.A.S. NEOXI <br>
+                    S.A.S. au capital de 40.000 € <br>
+                    Greenopolis <br>
+                    31, rue Saint Simon <br>
+                    69009 Lyon <br>
+                </p>
+                <p>R.C.S. LYON B 510 379 225 <br>
+                    Siret 510379225 00020 <br>
+                    Organisme agréé de formation 59510379225 <br>
+                    Code NAF -6202A <br>
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12 col-xs-12">
+                <h2>Hébergeur du site</h2>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
