2a60b8c72c691a2ad4c4a5b79e00fa9079cda89e
Adam <adam.benyahia@hotmail.fr>
2016-10-26 10:15:24 +0200

    include sql

--- src/NeoxisBundle/Controller/BackController.php
+++ src/NeoxisBundle/Controller/BackController.php
@@ -4,15 +4,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BackController extends Controller
 {
-    public function loginAction()
-    {
-        return $this->render('NeoxisBundle:Back:login.html.twig');
-    }
-
-    public function adminAction()
-    {
-        return $this->render('NeoxisBundle:Back:admin.html.twig');
-    }
 
     public function swiftmailerAction()
     {
