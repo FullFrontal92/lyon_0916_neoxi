f4463fdbf277c18f9d179154e57ca8ddc1739a3e
Yoann <yoann.gloaguen@gmail.com>
2016-10-26 17:27:49 +0200

    ajout du choix d'image dans le back d'ajout de news

--- web/js/back.js
+++ web/js/back.js
@@ -1,3 +1,5 @@
-function buttonInserSrc(id){
-
+function buttonInsertSrc(id){
+    var element = document.getElementsByClassName('imgBack');
+    var img = document.getElementsByClassName('srcImage');
+    img[0].value = element[id].src;
 }
